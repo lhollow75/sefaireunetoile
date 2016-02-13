c1e4e418803e7615e7389131f3a77de566b28b13
Matilde <Matilde>
2016-02-13 21:52:14 +0100

    add the maps library for research

--- js/javascript.js
+++ js/javascript.js
@@ -3,16 +3,16 @@ var marker;
 var myLatLng;
 	  
 	  
-window.onload = function() {
-  var startPos;
-  var geoSuccess = function(position) {
-    startPos = position;
-	console.log(startPos.coords.latitude+" / "+ startPos.coords.longitude);
-	initMap(startPos.coords.latitude, startPos.coords.longitude);
-  };
-  navigator.geolocation.getCurrentPosition(geoSuccess);
-  console.log(position);
-};
+// window.onload = function() {
+  // var startPos;
+  // var geoSuccess = function(position) {
+    // startPos = position;
+	// console.log(startPos.coords.latitude+" / "+ startPos.coords.longitude);
+	// initMap(startPos.coords.latitude, startPos.coords.longitude);
+  // };
+  // navigator.geolocation.getCurrentPosition(geoSuccess);
+  // console.log(position);
+// };
 
 function initMap(latitude, longitude) {
 	myLatLng = {lat: latitude, lng: longitude}
@@ -27,4 +27,28 @@ function initMap(latitude, longitude) {
 		map: map,
 		title: 'Hello World!'
 	});
+}
+
+var autocomplete;
+
+
+function geolocate() {
+  // Create the autocomplete object, restricting the search to geographical
+  // location types.
+  autocomplete = new google.maps.places.Autocomplete(
+      /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
+      {types: ['geocode']});
+
+  console.log(autocomplete);
+  
+  google.maps.event.addListener(autocomplete, 'place_changed', function () {
+            var place = autocomplete.getPlace();
+            console.log(place.name);
+            console.log(place.geometry.location.lat());
+            console.log(place.geometry.location.lng());
+            //alert("This function is working!");
+            //alert(place.name);
+           // alert(place.address_components[0].long_name);
+
+        });
 }
