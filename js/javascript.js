fa360eb1fd48d0bebf001a856da989c2e8f9761e
juliensallei <magna@live.fr>
2016-02-25 11:09:11 +0100

    Removing checking with console.log

--- js/javascript.js
+++ js/javascript.js
@@ -122,7 +122,6 @@ function recup_horaire_cinema(horaires){
 	var noMovies = true;
 	// console.log(horaires.feed.theaterShowtimes[0]);
 	showtimes = horaires.feed.theaterShowtimes[0];
-    console.log(showtimes.movieShowtimes.data);
 	if (showtimes.movieShowtimes != undefined){
 		
 		// Look all the movies showing in the theater
